0c0249ffd3a46c8757b1f30cbba571046d45aedc
Douglas R Colkitt <douglas.colkitt@gmail.com>
2019-02-21 21:26:56 +0000

    Re-write the envOption example to be clearer

--- examples/envOptions/sports-subcmd/env.sh
+++ examples/envOptions/sports-subcmd/env.sh
@@ -5,7 +5,6 @@ export SPORTS_TYPE=solo
 export SPORTS_COST=cheap
 
 function declareAnswer() {
-    local pick=
     echo "A good $SPORTS_TYPE sport for $SPORTS_GOAL is $1."
     echo "It's usually played $SPORTS_LOCATION"
     echo "The cost to get started is $SPORTS_COST"
